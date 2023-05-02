00fdc1a34a9af1056d71f8bfee3c62c44a92f630
victor marques <victormarques.ym@gmail.com>
2023-05-02 20:31:59 -0300

    tests

---
+++ tests/unit/controllers/controller.test.js
@@ -0,0 +1,54 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+const productsService = require('../../../src/services/productServcies');
+const productsController = require('../../../src/controllers/product.conrtroller')
+// const { connection } = require('../../../src/models/connection');
+const products = require('../models/moks/model.mock');
+
+chai.use(sinonChai);
+
+describe('Testando func do model de controllers', function () {
+  const req = {};
+  const res = {};
+
+  beforeEach(() => {
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('Testando se a func buscarProductos retorna todos os products', async function () {
+    sinon.stub(productsService, 'buscarProdutos').resolves({ message: products });
+
+    await productsController.listaProduct(req, res);
+
+    chai.expect(res.status).to.have.been.calledWith(200);
+    chai.expect(res.json).to.have.been.calledWithExactly(products);
+  })
+
+  it('Testando se a func buscarProdutosById retorna um erro com id inexistente', async function () {
+    req.params = { id: 5 };
+
+    sinon.stub(productsService, 'buscarProdutosById').resolves({ type: 404, message: 'Product not found' });
+
+    await productsController.getProdutoId(req, res);
+
+    chai.expect(res.status).to.have.been.calledWith(404);
+    chai.expect(res.json).to.have.been.calledWithExactly({ message: 'Product not found' });
+  })
+
+  it('Testando se a func buscarId retorna product especifico', async function () {
+    req.params = { id: 1 };
+
+    sinon.stub(productsService, 'buscarProdutosById').resolves({ message: products[2] });
+
+    await productsController.getProdutoId(req, res);
+
+    chai.expect(res.status).to.have.been.calledWith(200);
+    chai.expect(res.json).to.have.been.calledWithExactly(products[2]);
+  })
+})
---
+++ tests/unit/models/models.test.js
@@ -0,0 +1,30 @@
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+const chai = require('chai');
+const productModel = require('../../../src/models/productModel');
+const connection = require('../../../src/models/connection');
+const products = require('../models/moks/model.mock')
+
+
+const { expect } = chai;
+
+describe('model product test', function () {
+
+  afterEach(() => {
+    sinon.restore()
+  });
+
+  it('testando func da model  buscar product', async function () {
+    sinon.stub(connection, 'execute').resolves([products]);
+
+    const result = await productModel.findAll();
+    chai.expect(result).to.be.deep.equal(products);
+  });
+  it('Testando se a func findById se retorna product especifico', async function () {
+    sinon.stub(connection, 'execute').resolves([[products[2]]]);
+
+    const result = await productModel.findById(1);
+
+    chai.expect(result).to.be.deep.equal(products[2]);
+  })
+});
